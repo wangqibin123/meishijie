c04c225b30ed82f7eec94a9eaacd8705e5c3314b
wwh-xxm <93247140+wwh-xxm@users.noreply.github.com>
2022-02-06 17:10:14 +0800

    随便试试

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@
 module.exports = {
   publicPath: '/',
   devServer: {
+      open:true,
       proxy: {
           '/api': {  // http://localhost:8081/api/banner
               target: 'http://127.0.0.1:7001',  // http://127.0.0.1:7001/banner
